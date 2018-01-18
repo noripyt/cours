7d9b4cbec5446957594b397dd1de75152f926f7b
Bertrand Bordage <bordage.bertrand@gmail.com>
2018-01-18 01:02:25 +0100

    Typos and minor improvements in Python course.

--- python/ls.py
+++ python/ls.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
+from itertools import islice
 from pathlib import Path
+
+
+def to_dir_path(path_str):
+    dir_path = Path(path_str)
+    if dir_path.is_dir():
+        return dir_path
+    raise ArgumentTypeError('Le chemin n’est pas un dossier valide.')
+
 
 parser = ArgumentParser(
     description='Ce script liste les fichiers Python et leur taille.')
-parser.add_argument('chemin', default='.', nargs='?',
-                    help='Chemin vers le fichier')
+parser.add_argument('chemin', default='.', nargs='?', type=to_dir_path,
+                    help='Chemin vers le dossier à lister')
 parser.add_argument('--afficher-taille', action='store_true',
-                    help='Afficher la taille')
-parser.add_argument('--limite', type=int, default=100)
+                    help='Affiche la taille de chaque fichier')
+parser.add_argument('--limite', type=int, default=1000)
 args = parser.parse_args()
-paths = Path(args.chemin).glob('**/*.py')
 
-if args.afficher_taille:
-    def show_path(path):
+for path in islice(args.chemin.glob('**/*.py'), args.limite):
+    if args.afficher_taille:
         print('%6d octets' % path.stat().st_size, path)
-else:
-    def show_path(path):
+    else:
         print(path)
-
-
-for i, path in enumerate(paths):
-    if i == args.limite:
-        break
-    show_path(path)
